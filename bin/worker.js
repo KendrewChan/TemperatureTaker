0f23a168c456c9e9d855d4f8dd172c749dc5adbc
KendrewC <kendrewchanys@gmail.com>
2020-10-03 15:27:23 +0800

    cron test

--- bin/worker.js
+++ bin/worker.js
@@ -1,1 +1,14 @@
-console.log("Hello world!");
+const Puppeteer = require("./puppeteer");
+const DatabaseManager = require("./database");
+
+const temperature = (36.1 + Math.floor(Math.random() * 10) / 10)
+    .toFixed(1)
+    .toString();
+
+const kenID = 459499373;
+
+DatabaseManager.getUser(kenID, (err, netID, password) => {
+    Puppeteer.scrapeData(netID, password, temperature).catch((err) =>
+        console.log(err)
+    );
+});
