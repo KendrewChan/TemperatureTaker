3d31734004fe227ef97226f75fa01869ea64b6c7
KendrewC <kendrewchanys@gmail.com>
2020-10-15 21:11:00 +0800

    Add scrape by time

--- bin/worker.js
+++ bin/worker.js
@@ -2,6 +2,32 @@ const Puppeteer = require("../puppeteer");
 const DatabaseManager = require("../database");
 const Telegram = require("../telegram");
 
+const puppetScrape = (teleID, netID, password, morning, callback) => {
+    const temp = (36.1 + Math.floor(Math.random() * 10) / 10)
+        .toFixed(1)
+        .toString();
+
+    Puppeteer.scrapeTempTime(netID, password, temp, morning)
+        .then((page) => {
+            Telegram.sendMessage(
+                teleID,
+                `Your temperature of ${temp}°C has been set! ` +
+                    (morning ? `(AM)` : `(PM)`) +
+                    `😆`
+            );
+            callback(null, page);
+        })
+        .catch((err) => {
+            Telegram.sendMessage(
+                teleID,
+                `Sorry! An error has occurred while scraping ` +
+                    (morning ? `(AM)` : `(PM)`) +
+                    `🤒`
+            );
+            callback(err, null);
+        });
+};
+
 DatabaseManager.getAutoUsers((err, data) => {
     if (err) {
         console.log(err);
@@ -10,22 +36,16 @@ DatabaseManager.getAutoUsers((err, data) => {
         const netID = data.netID;
         const password = data.password;
 
-        const temperature = (36.1 + Math.floor(Math.random() * 10) / 10)
-            .toFixed(1)
-            .toString();
-
-        Puppeteer.scrapeData(netID, password, temperature)
-            .then((page) => {
-                Telegram.sendMessage(
-                    teleID,
-                    `Your temperature of ${temperature}°C has been set! 😆`
-                );
-            })
-            .catch((err) => {
-                Telegram.sendMessage(
-                    teleID,
-                    "Sorry! An error has occurred while scraping 🤒"
-                );
-            });
+        puppetScrape(teleID, netID, password, true, (err, page) => {
+            if (err) {
+                console.log(err);
+            } else {
+                puppetScrape(teleID, netID, password, false, (err, page) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                });
+            }
+        });
     }
 });
--- puppeteer.js
+++ puppeteer.js
@@ -5,55 +5,76 @@ const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 puppeteer.use(StealthPlugin());
 
 // Change this to open / close browser
-const closeBrowser = true;
+const closeBrowser = false;
 
 async function clickNavigate(page, selector) {
-  await Promise.all([
-    page.waitForNavigation(), // The promise resolves after navigation has finished
-    page.click(selector), // Clicking the link will indirectly cause a navigation
-  ]);
+    await Promise.all([
+        page.waitForNavigation(), // The promise resolves after navigation has finished
+        page.click(selector), // Clicking the link will indirectly cause a navigation
+    ]);
 }
 
 const pageHandler = {
-  getPage: async () => {
-    const browser = await puppeteer.launch({
-      headless: closeBrowser,
-      args: [
-        "--no-sandbox",
-        // "--disable-setuid-sandbox",
-        // "--disable-dev-shm-usage",
-      ],
-    });
-    const page = await browser.newPage();
-    await page.goto("https://myaces.nus.edu.sg/htd");
+    getPage: async () => {
+        const browser = await puppeteer.launch({
+            headless: closeBrowser,
+            args: [
+                "--no-sandbox",
+                // "--disable-setuid-sandbox",
+                // "--disable-dev-shm-usage",
+            ],
+        });
+        const page = await browser.newPage();
+        await page.goto("https://myaces.nus.edu.sg/htd");
 
-    return {
-      browser: browser,
-      page: page
-    };
-  },
-  nusLogin: async (page, id, pass) => {
-    await page.type("input[type=email]", id);
-    await page.type("input[type=password]", pass);
+        return {
+            browser: browser,
+            page: page,
+        };
+    },
+    nusLogin: async (page, id, pass) => {
+        await page.type("input[type=email]", id);
+        await page.type("input[type=password]", pass);
 
-    await clickNavigate(page, "#submitButton");
-  },
-  submitTemp: async (page, temp) => {
-    await page.click("input[name=symptomsFlag][value=N]");
-    await page.click("input[name=familySymptomsFlag][value=N]");
+        await clickNavigate(page, "#submitButton");
+    },
+    submitTemp: async (page, temp) => {
+        await page.click("input[name=symptomsFlag][value=N]");
+        await page.click("input[name=familySymptomsFlag][value=N]");
 
-    await page.type("input[name=temperature]", temp);
+        await page.type("input[name=temperature]", temp);
 
-    await clickNavigate(page, "input[name=Save]");
-  },
-  scrapeData: async (id, pass, temp) => {
-    const {browser, page} = await pageHandler.getPage();
-    await pageHandler.nusLogin(page, id, pass);
-    await pageHandler.submitTemp(page, temp);
+        await clickNavigate(page, "input[name=Save]");
+    },
+    scrapeData: async (id, pass, temp) => {
+        const { browser, page } = await pageHandler.getPage();
+        await pageHandler.nusLogin(page, id, pass);
+        await pageHandler.submitTemp(page, temp);
 
-    await browser.close();
-    return page; // This is kinda redundant at this point since browser already closed
-  },
+        await browser.close();
+        return page; // This is kinda redundant at this point since browser already closed
+    },
+    submitTempTime: async (page, temp, morning) => {
+        if (morning) {
+            await page.select("select[name=declFrequency]", "A");
+        } else {
+            await page.select("select[name=declFrequency]", "P");
+        }
+        await page.click("input[name=symptomsFlag][value=N]");
+        await page.click("input[name=familySymptomsFlag][value=N]");
+
+        await page.type("input[name=temperature]", temp);
+
+        await clickNavigate(page, "input[name=Save]");
+    },
+    scrapeTempTime: async (id, pass, temp, morning) => {
+        const { browser, page } = await pageHandler.getPage();
+        await pageHandler.nusLogin(page, id, pass);
+        await pageHandler.submitTempTime(page, temp, morning);
+
+        await browser.close();
+        return page; // This is kinda redundant at this point since browser already closed
+    },
 };
 
 module.exports = pageHandler;
